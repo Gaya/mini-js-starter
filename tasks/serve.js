1c7ad41188b17d1aaaec12880bb6db71d3644e47
Gaya <gaya.kessler@gmail.com>
2016-02-11 13:13:40 +0100

    pass browserSync to browserify

--- tasks/serve.js
+++ tasks/serve.js
@@ -2,7 +2,7 @@ var browserSync = require('browser-sync').create();
 var Browserify = require('./browserify');
 
 //initialize browserify
-Browserify('./src/index', './dist/', 'bundle.js');
+Browserify('./src/index', './dist/', 'bundle.js', browserSync);
 
 //serve stuff
 browserSync.init({
