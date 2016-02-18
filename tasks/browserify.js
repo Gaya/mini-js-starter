cda7c9f9719fdc1e076ac9c5b442f8f56cd093f6
Gaya <gaya.kessler@gmail.com>
2016-02-18 08:40:50 +0100

    enable source maps in browserify

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -7,6 +7,7 @@ module.exports = function(src, dist, name, browserSync) {
     cache: {},
     packageCache: {},
     plugin: [watchify],
+    debug: true,
   }).transform('babelify', { presets: ['es2015'] });
 
   //create dir before compiling
